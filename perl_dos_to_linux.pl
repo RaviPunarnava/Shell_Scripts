7df719e605ba613942aea24add639b6799144234
H R <shr@EBR-SHR2-MAC.local>
2019-10-03 11:15:40 +0530

    l Please enter the commit message for your changes. Lines starting dos to linux file conversion with EOF# with '#' will be ignored, and an empty message aborts the commit.

---
+++ perl_dos_to_linux.pl
@@ -0,0 +1,4 @@
+#this perl script converts windows type of file to Linux type of file
+#winfile is the text file in windows type formatting or windows type end of line
+#unix file is text file with linux type end of file
+perl -p -e 's/\r$//' < winfile.txt > unixfile.txt
